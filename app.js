ff4cc3f14464a7ab474a7bc73eca2157eb2b5972
Emily Zugel <91398870+emilyzugel@users.noreply.github.com>
2023-05-23 23:49:53 -0300

    New design

---
+++ app.js
@@ -0,0 +1,5 @@
+const errorMsg = document.querySelector("#error")
+
+function error(){
+    errorMsg.textContent = "Sorry, I'm still learning JavaScript. Some features might not work yet :c"
+}
--- script.js
+++
@@ -1,6 +0,0 @@
-let Error = document.getElementById("error");
-
-function errorMsg() {
-    Error.textContent =
-        "Sorry, I'm still leraning how to work with back-end. Thanks for being pacient :)";
-}
