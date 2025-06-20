2729079579c961ce96fbe0b1f7c9990e41f9b179
Emily Zugel <91398870+emilyzugel@users.noreply.github.com>
2025-06-20 16:38:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-const errorMsg = document.querySelector("#error")
-
-function error(){
-    errorMsg.textContent = "Sorry, I'm still learning JavaScript. Some features might not work yet :c"
-}
+//const errorMsg = document.querySelector("#error")
